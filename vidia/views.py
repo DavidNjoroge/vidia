7a31b1c2e3135d4d872d764fbb08a09e723d3964
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-05 18:35:47 +0300

    Create new_tag view function
    Update submit_project view function
    * add new tag form

--- vidia/views.py
+++ vidia/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Project, Tag
-from .forms import ProjectForm
+from .forms import ProjectForm, TagForm
 
 
 # index page
@@ -17,14 +17,30 @@ def submit_project(request):
     user = request.user
 
     new_project_form = ProjectForm(request.POST, request.FILES)
+    new_tag_form = TagForm(request.POST)
     if user.is_authenticated and new_project_form.is_valid():
         project = new_project_form.save(commit=False)
-        project.user_id = user
+        project.user_id = user.id
         project.save()
-
+        new_project_form.save_m2m()
         return redirect(index)
-
     else:
         new_project_form = ProjectForm(request.POST)
+        new_tag_form = TagForm(request.POST)
 
-    return render(request, 'new_project.html', {"new_project_form": new_project_form})
+    return render(request, 'new_project.html', {"new_project_form": new_project_form, "new_tag_form": new_tag_form})
+
+
+@login_required(login_url="/accounts/login")
+def create_tag(request):
+    user = request.user
+    new_tag_form = TagForm(request.POST)
+
+    if user.is_authenticated and new_tag_form.is_valid():
+        tag = new_tag_form.save(commit=False)
+        tag.user = user
+        tag.save()
+        return redirect(submit_project)
+    else:
+        new_tag_form = TagForm(request.POST)
+    return render(request, 'new_tag.html', {"new_tag_form": new_tag_form})

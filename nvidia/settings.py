a16806b180dc223aedf6a4732051f73ee3bf4ae6
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-05 18:41:02 +0300

    update DEBUG settings

--- nvidia/settings.py
+++ nvidia/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 

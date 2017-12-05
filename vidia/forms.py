dc511e0c6239bad46dead4871702cdc0ee91359b
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-05 18:40:35 +0300

    Create tag form update Project form - m2m attribute

--- vidia/forms.py
+++ vidia/forms.py
@@ -5,4 +5,13 @@ from .models import Project, Profile, Tag
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        exclude = ['post_date', 'user']
+        exclude = ['post_date', 'user', ]
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        exclude = ['user', ]

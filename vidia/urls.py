a84db38081322a4a864405dcd93094c13a7917e8
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-05 19:03:43 +0300

    Add create_project_tag url

--- vidia/urls.py
+++ vidia/urls.py
@@ -6,7 +6,8 @@ from .import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^submit_project/', views.submit_project, name='submit_project'),
-    url(r'^create_tag/', views.create_tag, name='create_tag')
+    url(r'^create_tag/', views.create_tag, name='create_tag'),
+    url(r'^create_project_tag/', views.create_project_tag, name='create_project_tag')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,

d9564793500afdabb717a7eaeeb2745e59de4776
David Mwangi <njorogedavid13@gmail.com>
2017-12-06 15:57:21 +0300

    create test case for save project method

--- vidia/tests.py
+++ vidia/tests.py
@@ -6,12 +6,15 @@ from django.contrib.auth.models import User
 
 class ProjectTestClass(TestCase):
     def setUp(self):
-        # self.james = User(username='davy',password='boomshakalaka')
-        # self.james.save()
-        self.game = Project(title='dsfgff',description='dfsfasd',github_link='sdfsdfsdf',deployed_link='sdfdafgdfs',picture='sdfsfsdf')
-        # self.game.save()
+        self.james = User(username='davy',password='boomshakalaka')
+        self.james.save()
+        self.game = Project(user=self.james,title='dsfgff',description='dfsfasd',github_link='sdfsdfsdf',deployed_link='sdfdafgdfs',picture='sdfsfsdf')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.game,Project))
 
-    
+    def test_save_method(self):
+        self.game.save_project()
+        
+        projects=Project.objects.all()
+        self.assertTrue(len(projects)>0)
